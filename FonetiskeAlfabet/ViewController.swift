fbbc483d5312180cc61830a9bc090ffc2a5216e8
Soren <soren1146@gmail.com>
2018-08-17 16:18:06 +0200

    Initial Commit

---
+++ FonetiskeAlfabet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FonetiskeAlfabet
+//
+//  Created by Søren Møller Gade Hansen on 17/08/2018.
+//  Copyright © 2018 Søren Møller Gade Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
